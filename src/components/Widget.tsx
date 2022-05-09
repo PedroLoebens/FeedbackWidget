68970488b30d77519339e6dccf52bfd6e2f6985d
Pedro Loebens <loebens@outlook.com>
2022-05-09 09:38:57 -0300

    implementação Content e form do Widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,11 +1,14 @@
 import { ChatTeardropDots } from 'phosphor-react';
 import { Popover } from '@headlessui/react' //biblioteca utilizada para facilitar a acessibilidade da aplicação
+import { WidgetForm } from './Widgetform';
 
 export function Widget() {
    
    return (
-     <Popover className="absolute bottom-5 right-5 ">
-         <Popover.Panel>Helo World</Popover.Panel>
+     <Popover className="absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end">
+         <Popover.Panel>
+            <WidgetForm />
+         </Popover.Panel>
          <Popover.Button className="bg-violet-500 rounded-full px-3 h-12 text-white flex items-center group ">
             <ChatTeardropDots className="w-6 h-6" />
 
---
+++ src/components/Widgetform/ScreenshotButton.tsx
@@ -0,0 +1,56 @@
+import { useCallback, useState } from "react";
+import html2canvas from 'html2canvas'
+import { Camera, Trash } from "phosphor-react";
+import { Loading } from "../Loading";
+
+interface ScreenshotButtonProps {
+   screenshot: string | null
+   onScreenshotTook: (screenshot: string | null) => void
+}
+
+export function ScreenshotButton({
+   screenshot,
+   onScreenshotTook
+}: ScreenshotButtonProps) {
+   const [isTakingScreenshot, setIsTakingScreenShot] = useState(false)
+
+   const handleTakeScreenshot = useCallback(async () => {
+      setIsTakingScreenShot(true)
+
+      const canvas = await html2canvas(document.querySelector('html')!)
+      const base64image = canvas.toDataURL('image/png')
+
+      console.log(base64image) //imagem da página
+
+      onScreenshotTook(base64image)
+
+      setIsTakingScreenShot(false)
+   }, [])
+
+   if (screenshot) {
+      return (
+         <button
+            type="button"
+            onClick={() => onScreenshotTook(null)}
+            style={{
+               backgroundImage: `url(${screenshot})`,
+               backgroundPosition: 'right bottom', //atributo para exibir o canto inferior esquerdo, pois o só tem o widget na página (DESNECESSÀRIO)
+               backgroundSize: 180, //atributo para exibir o canto inferior esquerdo, pois o só tem o widget na página (DESNECESSÀRIO)
+            }}
+            className="p-1 w-10 h10 rounded-md border-transparent flex justify-end items-end text-zinc-500 dark:text-zinc-400 hover:text-zinc-800 dark:hover:text-zinc-100 transition-colors border-[1px] border-zinc-300 dark:border-zinc-600"
+         >
+            <Trash weight="fill" />
+         </button>
+      )
+   }
+
+   return (
+      <button
+         type="button"
+         onClick={handleTakeScreenshot}
+         className="p-2 bg-zinc-100 dark:bg-zinc-800 rounded-md border-transparent hover:bg-zinc-200 dark:hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-500"
+      >
+         {isTakingScreenshot ? <Loading /> : <Camera className="w-6 h-6 text-zinc-800 dark:text-zinc-100" />}
+      </button>
+   )
+}
